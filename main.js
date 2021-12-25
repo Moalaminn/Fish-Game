228a5cee33158ff499fac65b69707252820402a2
moalaminn <moalaminn@gmail.com>
2021-12-25 17:32:50 +0000

    third  commit

--- main.js
+++ main.js
@@ -1,68 +1,68 @@
 // Canvas Setup
-const canvas = document.getElementById('canvas-1');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas-1");
+const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 500;
 
 let score = 0;
-let gameFrame = 0; 
-ctx.font = '50px Georgia';
+let gameFrame = 0;
+ctx.font = "50px Georgia";
 
 // Mouse Interactivity
 let canvasPosition = canvas.getBoundingClientRect();
 const mouse = {
-    x: canvas.width/2,
-    y: canvas.height/2,
-    click: false
-}
-canvas.addEventListener('mousedown',function(event) {
-    mouse.click = true;
-    mouse.x = event.x - canvasPosition.left;
-    mouse.y = event.y - canvasPosition.top;
-    console.log(mouse.x, mouse.y);
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  click: false,
+};
+canvas.addEventListener("mousedown", function (event) {
+  mouse.click = true;
+  mouse.x = event.x - canvasPosition.left;
+  mouse.y = event.y - canvasPosition.top;
+  console.log(mouse.x, mouse.y);
 });
-canvas.addEventListener('mouseup', function(){
-    mouse.click = false;
+canvas.addEventListener("mouseup", function () {
+  mouse.click = false;
 });
 
 // Player
 
 class Player {
-    constructor() {
-        this.x = canvas.width / 2;
-        this.y = canvas.height / 2;
-        this.radius = 50;
-        this.angle = 0;
-        this.frameX = 0;
-        this.frameY = 0;
-        this.frame = 0;
-        this.spriteWidth = 498;
-        this.spriteHeight = 327;
+  constructor() {
+    this.x = canvas.width / 2;
+    this.y = canvas.height / 2;
+    this.radius = 50;
+    this.angle = 0;
+    this.frameX = 0;
+    this.frameY = 0;
+    this.frame = 0;
+    this.spriteWidth = 498;
+    this.spriteHeight = 327;
+  }
+  update() {
+    const dx = this.x - mouse.x;
+    const dy = this.y - mouse.y;
+    if (mouse.x != this.x) {
+      this.x -= dx / 30;
     }
-    update() {
-        const dx = this.x - mouse.x;
-        const dy = this.y - mouse.y; 
-        if (mouse.x != this.x) {
-            this.x -= dx/30;
-        }
-        if (mouse.y != this.y) {
-            this.y  -= dy/30;
-        }
+    if (mouse.y != this.y) {
+      this.y -= dy / 30;
     }
-    draw() {
-        if (mouse.click) {
-            ctx.lineWidth = 0.2;
-            ctx.beginPath();
-            ctx.moveTo(this.x, this.y);
-            ctx.lineTo(mouse.x, mouse.y);
-            ctx.stroke();
-        }
-        ctx.fillStyle = 'red';
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.closePath();
+  }
+  draw() {
+    if (mouse.click) {
+      ctx.lineWidth = 0.2;
+      ctx.beginPath();
+      ctx.moveTo(this.x, this.y);
+      ctx.lineTo(mouse.x, mouse.y);
+      ctx.stroke();
     }
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+  }
 }
 
 const player = new Player();
@@ -71,45 +71,63 @@ const player = new Player();
 
 const bubblesArray = [];
 class Bubble {
-    constructor(){
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.radius = 50;
-        this.speed = Math.random() * 5 + 1;
-        this.distance; 
-    }
-    update(){
-        this.y -= this.speed;
-
-    }
-    draw(){
-        ctx.fillStyle = 'blue';
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.closePath();
-        ctx.stroke();
-    }
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = canvas.height + 100;
+    this.radius = 50;
+    this.speed = Math.random() * 5 + 1;
+    this.distance;
+    this.counted = false;
+  }
+  update() {
+    this.y -= this.speed;
+    const dx = this.x - player.x;
+    const dy = this.y - player.y;
+    this.distance = Math.sqrt(dx * dx + dy * dy);
+  }
+  draw() {
+    ctx.fillStyle = "blue";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+    ctx.stroke();
+  }
 }
 
 function handleBubbles() {
-    if (gameFrame % 50 == 0) {
-        bubblesArray.push(new Bubble());
-        console.log(bubblesArray.length);
-    }
+  if (gameFrame % 50 == 0) {
+    bubblesArray.push(new Bubble());
+    console.log(bubblesArray.length);
+  }
+  for (let i = 0; i < bubblesArray.length; i++) {
+    bubblesArray[i].update();
+    bubblesArray[i].draw();
+  }
     for (let i = 0; i < bubblesArray.length; i++) {
-        bubblesArray[i].update();
-        bubblesArray[i].draw();
+      if (bubblesArray[i].y < 0 - bubblesArray[i].radius * 2) {
+        bubblesArray.splice(i, 1);
+      }
+      if (bubblesArray[i].distance < bubblesArray[i].radius + player.radius) {
+        console.log("collision");
+        if (!bubblesArray[i].counted) {
+            score++;
+            bubblesArray[i].counted = true;
+            bubblesArray.splice(i,1);
+        }
+      }
     }
 }
 
 // Animation Loop
 function animate() {
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    handleBubbles();
-    player.update();
-    player.draw();
-    gameFrame++;
-    requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handleBubbles();
+  player.update();
+  player.draw();
+  ctx.fillStyle = "black";
+  ctx.fillText("score: " + score, 10, 50);
+  gameFrame++;
+  requestAnimationFrame(animate);
 }
 animate();
